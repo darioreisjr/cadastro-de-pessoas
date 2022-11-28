1b6dd877cfa46fa9426840c611493639e101de03
Dario Reis <guttyerresjunior1995@gmail.com>
2022-11-28 18:23:23 -0300

    feat:  Adicionado tema claro e provider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ export const AppRoutes = () => {
     return (
         <Routes>
             <Route path='/pagina-inicial' element={
-            <Button>Teste</Button>
+            <Button variant='contained' color='primary'>Teste</Button>
             } />
 
             <Route path='*' element={<Navigate to={'/pagina-inicial'} />} />
